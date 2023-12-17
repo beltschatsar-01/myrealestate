80670222f2e4c85a971fa055a2aa8e27fbf4a20a
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-12-17 16:32:41 +0300

    Setup of all dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Profile />} />
+
+
+      </Routes>
+    </Router>
+  );
+}
